f8ddc320ea82f89a2e2728c337dd427fa9bcb834
Athif Irshad <athifirshad@hotmail.com>
2024-11-06 17:56:53 +0000

    Enhance cmdAdd with improved error handling, debug logging, and container namespace support; update generateVethName to include namespace; add sequence diagram to ReadMe for networking flow

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"net"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
 	current "github.com/containernetworking/cni/pkg/types/100"
 	"github.com/containernetworking/cni/pkg/version"
+	"github.com/containernetworking/plugins/pkg/ip"
 	"github.com/containernetworking/plugins/pkg/ipam"
+	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/vishvananda/netlink"
 )
 
@@ -44,73 +47,92 @@ func main() {
 
 }
 
-// Update cmdAdd with better error handling
 func cmdAdd(args *skel.CmdArgs) error {
-	// Add debug logging
 	fmt.Fprintf(os.Stderr, "Debug: Starting ADD for container %s\n", args.ContainerID)
 
 	// Parse network configuration
 	conf := &NetConf{}
 	if err := json.Unmarshal(args.StdinData, conf); err != nil {
-		return fmt.Errorf("failed to parse network config: %v", err)
+		return fmt.Errorf("failed to parse config: %v", err)
 	}
 
-	// Generate interface names
-	hostVethName := generateVethName(args.ContainerID, args.IfName)
-	fmt.Fprintf(os.Stderr, "Debug: args.ContainerID = %s\n", args.ContainerID)
-	fmt.Fprintf(os.Stderr, "Debug: hostVethName = %s\n", hostVethName)
-	contVethName := args.IfName
-
-	// Check if veth already exists and clean up if needed
-	if link, err := netlink.LinkByName(hostVethName); err == nil {
-		if err := netlink.LinkDel(link); err != nil {
-			return fmt.Errorf("failed to delete existing veth %q: %v", hostVethName, err)
-		}
-	}
-
-	// Create veth pair
-	veth := &netlink.Veth{
-		LinkAttrs: netlink.LinkAttrs{
-			Name: hostVethName,
-			MTU:  conf.MTU,
-		},
-		PeerName: contVethName,
-	}
-
-	// Add more verbose error handling for veth creation
-	if err := netlink.LinkAdd(veth); err != nil {
-		fmt.Fprintf(os.Stderr, "Debug: Failed to create veth pair: %v\n", err)
-		// Try cleanup before returning error
-		if link, err := netlink.LinkByName(hostVethName); err == nil {
-			netlink.LinkDel(link)
-		}
-		return fmt.Errorf("failed to create veth pair: %v", err)
-	}
-
-	fmt.Fprintf(os.Stderr, "Debug: Successfully created veth pair %s <-> %s\n",
-		hostVethName, contVethName)
-
-	// Get host veth interface
-	hostVeth, err := netlink.LinkByName(hostVethName)
+	// Get container network namespace
+	containerNS, err := ns.GetNS(args.Netns)
 	if err != nil {
-		return fmt.Errorf("failed to lookup host veth %q: %v", hostVethName, err)
+		return fmt.Errorf("failed to get container netns: %v", err)
+	}
+	defer containerNS.Close()
+
+	// Create interfaces within container namespace
+	var hostInterface, containerInterface *current.Interface
+	err = containerNS.Do(func(netNS ns.NetNS) error {
+		// Setup veth pair inside container namespace
+		hostVeth, containerVeth, err := ip.SetupVeth(args.IfName, conf.MTU, generateVethName(args.ContainerID, args.IfName), netNS)
+		if err != nil {
+			return fmt.Errorf("failed to setup veth: %v", err)
+		}
+
+		hostInterface = &current.Interface{
+			Name: hostVeth.Name,
+			Mac:  hostVeth.HardwareAddr.String(),
+		}
+		containerInterface = &current.Interface{
+			Name:    containerVeth.Name,
+			Mac:     containerVeth.HardwareAddr.String(),
+			Sandbox: netNS.Path(),
+		}
+		return nil
+	})
+	if err != nil {
+		return err
 	}
 
-	// Bring host veth up
-	if err := netlink.LinkSetUp(hostVeth); err != nil {
-		return fmt.Errorf("failed to set %q up: %v", hostVethName, err)
-	}
+	fmt.Fprintf(os.Stderr, "Debug: Created veth pair %s <-> %s\n",
+		hostInterface.Name, containerInterface.Name)
 
 	// Call IPAM plugin
 	r, err := ipam.ExecAdd(conf.IPAM.Type, args.StdinData)
 	if err != nil {
-		return fmt.Errorf("IPAM add failed: %v", err)
+		return fmt.Errorf("IPAM failed: %v", err)
 	}
 
-	// Convert result to current version
+	// Convert to current version
 	result, err := current.NewResultFromResult(r)
 	if err != nil {
-		return fmt.Errorf("failed to convert IPAM result: %v", err)
+		return fmt.Errorf("failed to convert result: %v", err)
+	}
+
+	// Configure IP address in container namespace
+	err = containerNS.Do(func(netNS ns.NetNS) error {
+		// Get container interface
+		containerVeth, err := netlink.LinkByName(containerInterface.Name)
+		if err != nil {
+			return fmt.Errorf("failed to lookup %q: %v", containerInterface.Name, err)
+		}
+
+		// Add IP addresses to container interface
+		for _, ipc := range result.IPs {
+			addr := &netlink.Addr{
+				IPNet: &net.IPNet{
+					IP:   ipc.Address.IP,
+					Mask: ipc.Address.Mask,
+				},
+			}
+			if err := netlink.AddrAdd(containerVeth, addr); err != nil {
+				return fmt.Errorf("failed to add IP addr %v to %q: %v", addr, containerInterface.Name, err)
+			}
+			fmt.Fprintf(os.Stderr, "Debug: Added IP %v to %s\n", addr, containerInterface.Name)
+		}
+
+		// Set container interface up
+		if err := netlink.LinkSetUp(containerVeth); err != nil {
+			return fmt.Errorf("failed to set %q up: %v", containerInterface.Name, err)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return err
 	}
 
 	return types.PrintResult(result, conf.CNIVersion)
@@ -119,7 +141,6 @@ func cmdAdd(args *skel.CmdArgs) error {
 // Update generateVethName to include namespace
 func generateVethName(containerID string, ifName string) string {
 	h := sha1.New()
-	// Add random component to avoid collisions
 	h.Write([]byte(fmt.Sprintf("%s-%s-%d", containerID, ifName, os.Getpid())))
 	sha := hex.EncodeToString(h.Sum(nil))
 	return fmt.Sprintf("veth%s", sha[:11])
